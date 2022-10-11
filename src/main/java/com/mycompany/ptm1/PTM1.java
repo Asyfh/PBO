25eb91b782e31c96dfc7b110c0c300443d18082c
unknown <21103001@ittekom-pwt.ac.id>
2022-10-11 11:08:34 +0700

    Komentar

---
+++ src/main/java/com/mycompany/ptm1/PTM1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ptm1;
+
+/**
+ *
+ * @author LAB PROGRAMING
+ */
+public class PTM1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
